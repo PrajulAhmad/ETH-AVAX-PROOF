d08c731efc762e2808426ece504a7b718bac76f3
Prajul Ahmad <129886083+PrajulAhmad@users.noreply.github.com>
2024-06-21 21:18:37 +0530

    Update FuncErr_Assessment.sol

--- FuncErr_Assessment/FuncErr_Assessment.sol
+++ FuncErr_Assessment/FuncErr_Assessment.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+contract FuncErr_Assessment
+{
+    uint public value;
+    address public owner;
+
+    constructor()
+    {
+        owner = msg.sender;
+    }
+    modifier realOwner() 
+    {
+        require(msg.sender == owner, "Only owner can access");
+        _;
+    }
+
+    function setValue(uint _value) public realOwner
+    {
+        require(_value >= 0, "Value must not be negative");
+        assert(_value <= 500);  //value should be under 500
+        value = _value;
+    }
+
+    function resetValue() public realOwner 
+    {
+        
+        if (value == 0) 
+        {
+            revert("Value is already zero");
+        }
+        value = 0;
+    }
+}
